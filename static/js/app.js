034d1d8c1ebe22f4ebf04a88c55693267647171b
angelilucila <73862577+angelilucila@users.noreply.github.com>
2021-03-06 20:41:36 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,4 @@
 
-
 // 1. Populate Table
 
 // From data.js
@@ -12,7 +11,7 @@ var tbody = d3.select("tbody");
 console.log(data);
 
 // Append table row for each datapoint
-data.forEach(function(oneDatapoint) {
+tableData.forEach(function(oneDatapoint) {
   // Create a new row
   var tr = tbody.append('tr');
   // Append table data for each table row
@@ -23,18 +22,60 @@ data.forEach(function(oneDatapoint) {
   )
 })
 
+// Clear table before re-rendering
 
-// 2. Event Handler 
-// **UFO-level-1** : date/time input
+
+// 2. Event Handlers
 
 // grab references to the input element and the output div
-var input_element = d3.select('#form-control');
-var output_element = d3.select('.ufo-table');
+// grab references to the input element and the output div
 
-// // Function to filter from user input
-// function filterto(date) {
-//   return date.
-// };
+var button = d3.select("#filter-btn");
+
+
+// Use D3 `.on` to attach a click handler for the upvote
+button.on("click", function() {
+  // retreive datetime
+  var inputDate = d3.select("#datetime").property('value');
+  var dateCalled = data.filter( selectedData => selectedData.datetime === inputDate);
+  tbody.html("");
+
+  dateCalled.forEach(function(selectedDatePoint) {
+    var tr = tbody.append('tr');
+    // Append table data for each table row
+    Object.keys(selectedDatePoint).forEach(
+      function(key) {
+        tr.append('td').text(selectedDatePoint[key]);
+  }) 
+  console.log(inputDate)
+}); 
+});
+
+
+
+// var filterData = tableData;
+// filterData.forEach(function(Date) {
+//   return dataCalled
+// }); 
+
+// var city = d3.select(".city");
+// var state = d3.select("state");
+// var shape = d3.select(".shape");
+
+// // confused on output!! 
+// var output = d3.select("tbody");
+
+
+// Function to filter from user input
+ //function filterto(ufo) {
+  //  //return ufo.
+  //  // grab the value of the input field
+  //  var value = d3.event.target.value;
+
+  // // clear the existing output
+  // output.html("");
+
+  //  //};
 
 // // Function to handle input change
 // function handleChange(event) {
@@ -48,13 +89,15 @@ var output_element = d3.select('.ufo-table');
 
 //   // Set the output text 
 
-// }
+//
 
 // // Attach an event to detect changes to the input field. 
 // input_element.on("change", handleChange);
 
+
 // **UFO-level-2** : multiple criteria input
+// BONUS: Handles 5 filter criterias at once 
 
 
 
-  
+// Prevents the Page from reloading using D3
